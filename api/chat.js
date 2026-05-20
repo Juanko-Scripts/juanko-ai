deea99263a6cc59d64b5c34be820dd173c4101e9
Juanko-Scripts <167231388+Juanko-Scripts@users.noreply.github.com>
2026-05-20 01:13:17 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,20 +1,67 @@
-let latestImage = "";
+import fs from "fs";
+import path from "path";
+
+const filePath =
+path.join(process.cwd(), "image.json");
 
 export default async function handler(req, res) {
 
 if(req.method === "POST") {
 
-latestImage = req.body.image || "";
+try{
+
+const body = req.body;
+
+fs.writeFileSync(
+filePath,
+JSON.stringify({
+image: body.image || ""
+})
+);
 
 return res.status(200).json({
 success:true
 });
 
+}catch(err){
+
+return res.status(500).json({
+success:false,
+error:String(err)
+});
+
 }
 
-res.status(200).json({
+}
+
+try{
+
+if(!fs.existsSync(filePath)){
+
+return res.status(200).json({
 success:true,
-image:latestImage
+image:""
 });
+
+}
+
+const data =
+JSON.parse(
+fs.readFileSync(filePath,"utf8")
+);
+
+return res.status(200).json({
+success:true,
+image:data.image || ""
+});
+
+}catch(err){
+
+return res.status(500).json({
+success:false,
+error:String(err)
+});
+
+}
 
 }
