56b1f1cfe3f2e35a4d167b93d39ba63cc18d284d
Juanko-Scripts <167231388+Juanko-Scripts@users.noreply.github.com>
2026-05-19 23:54:02 -0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
 
-  const prompt = req.query.prompt || "anime girl";
+  const prompt = req.query.prompt || "JM XP";
 
   const image =
-  `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`;
+  `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?width=1024&height=1024&seed=${Date.now()}`;
 
   res.status(200).json({
     success: true,
